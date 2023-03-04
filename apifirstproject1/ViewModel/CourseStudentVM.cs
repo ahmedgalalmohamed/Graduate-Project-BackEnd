097821701abf73d5c0165bfdb225f3bf6df129bc
IbrahimMZeid <abrahymzyd43@gmail.com>
2023-03-04 16:03:31 +0200

    modify getcourse for instructor and update min & max std

--- apifirstproject1/ViewModel/CourseStudentVM.cs
+++ apifirstproject1/ViewModel/CourseStudentVM.cs
@@ -8,6 +8,8 @@
         public int? ProId { get; set; }
         public int? MyTeam { get; set; }
         public int? TeamLeader { get; set; }
+        public int? MinStd { get; set; }
+        public int? MaxStd { get; set; }
         public string Name { get; set; }
         public bool ? IsGraduate { get; set; }
         public string Description { get; set; }
