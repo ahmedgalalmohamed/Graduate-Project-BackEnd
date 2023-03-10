9e22010648d4773ac554c7437a996b2d3ff2b977
IbrahimMZeid <abrahymzyd43@gmail.com>
2023-03-10 19:25:52 +0200

    make the team isComplete = false after student leave the team

--- apifirstproject1/Controllers/TeamController.cs
+++ apifirstproject1/Controllers/TeamController.cs
@@ -121,7 +121,7 @@ namespace Graduate_Project_BackEnd.Controllers
             }
             if (currentUser.Id == id)
             {
-                var team = DB.Teams.Where(t => t.Id == t_id).Select(t => new { t.Id, t.CourseID }).ToList();
+                var team = DB.Teams.Where(t => t.Id == t_id).Select(t => new { t.Id, t.Name, t.LeaderID, t.CourseID, t.IsComplete }).ToList();
                 if (team.Count > 0)
                 {
                     var std = DB.Courses_Students.Single(s => s.StudentID == currentUser.Id && s.CourseID == team[0].CourseID);
@@ -136,6 +136,15 @@ namespace Graduate_Project_BackEnd.Controllers
                         DB.Notifications.RemoveRange(notifications);
                         DB.SaveChanges();
                     }
+                    DB.Teams.Update(new TeamModel()
+                    {
+                        Id = team[0].Id,
+                        Name = team[0].Name,
+                        CourseID = team[0].CourseID,
+                        LeaderID = team[0].LeaderID,
+                        IsComplete = false,
+                    });
+                    DB.SaveChanges();
                     return Json(new { state = true, msg = "Done" });
                 }
             }
