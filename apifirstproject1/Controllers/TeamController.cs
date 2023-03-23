8df094a6cc9f64647df410d9292d5d37bde86677
IbrahimMZeid <abrahymzyd43@gmail.com>
2023-03-23 22:39:13 +0200

    return role with data GetMyTeam method

--- apifirstproject1/Controllers/TeamController.cs
+++ apifirstproject1/Controllers/TeamController.cs
@@ -101,9 +101,9 @@ namespace Graduate_Project_BackEnd.Controllers
                 if (team.Count == 0)
                     return Json(new { state = false, msg = "Failed to get data" });
             }
-            var leader = DB.Courses_Students.Where(cs => cs.TeamID == id && cs.Team.LeaderID == cs.StudentID).Select(s => new { s.StudentID, s.Student.Name, s.Student.Email, teamId = s.TeamID, s.CourseID, course = s.Course.Name, team = s.Team.Name, s.Team.IsComplete, s.Student.img });
-            var members = DB.Courses_Students.Where(cs => cs.TeamID == id && cs.Team.LeaderID != cs.StudentID).Select(s => new { s.StudentID, s.Student.Name, s.Student.Email, teamId = s.TeamID, s.Student.img });
-            var professor = DB.Teams.Where(t => t.Id == id && t.ProfID != null).Select(p => new { p.Prof.Id, p.Prof.Name, p.Prof.Email, p.Prof.img }).ToList();
+            var leader = DB.Courses_Students.Where(cs => cs.TeamID == id && cs.Team.LeaderID == cs.StudentID).Select(s => new { s.StudentID, s.Student.Name, s.Student.Email, teamId = s.TeamID, s.CourseID, course = s.Course.Name, team = s.Team.Name, s.Team.IsComplete, s.Student.img, role = "student" });
+            var members = DB.Courses_Students.Where(cs => cs.TeamID == id && cs.Team.LeaderID != cs.StudentID).Select(s => new { s.StudentID, s.Student.Name, s.Student.Email, teamId = s.TeamID, s.Student.img, role = "student" });
+            var professor = DB.Teams.Where(t => t.Id == id && t.ProfID != null).Select(p => new { p.Prof.Id, p.Prof.Name, p.Prof.Email, p.Prof.img, role = "proffessor" }).ToList();
             if (members != null)
             {
                 return Json(new { state = true, msg = "Success", data = new { leader, members, professor = professor.Count == 0 ? null : professor[0] } });
